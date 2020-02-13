bbd3c96cd01731286dc5da31f2d042da9f580cf9
Jack Markrest <if1033103081@gmail.com>
2020-02-13 13:46:56 +0800

    :and Install.sh

---
+++ Install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sudo apt install atool bsdtar unrar 7z pdftotext \
+	mutool exiftool transmission-gtk odt2txt w3m w3m-img \
+	convert ffmpegthumbnailer pdftoppm unzip \
+	highlight exiftool mediainfo 
