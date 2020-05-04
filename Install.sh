49d5c5b8314ab10ae8ef3787008163f9aaabc26d
aodong.qin <if1033103081@gmail.com>
2020-05-04 15:25:20 +0800

    debian update

--- Install.sh
+++ Install.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
-sudo apt install atool bsdtar unrar 7z pdftotext \
-	mutool exiftool transmission-gtk odt2txt w3m w3m-img \
-	convert ffmpegthumbnailer pdftoppm unzip \
-	highlight exiftool mediainfo 
+
+case `uname -s` in
+	Linux)
+
+		if cat /etc/os-release | grep debian1 ;then
+			sudo apt install -y atool bsdtar unrar-free p7zip \
+				poppler-utils \
+				mupdf-tools exiftool transmission-gtk \
+				odt2txt w3m w3m-img ffmpegthumbnailer \
+				graphicsmagick-imagemagick-compat \
+				unzip highlight exiftool mediainfo
+
+		else
+			echo "not debian"
+
+		fi
+		break
+		;;
+	*)
+		echo "not a linux distribution"
+esac
